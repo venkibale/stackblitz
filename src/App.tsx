817fe9375566487077cb4c8d851f2ed9ada1b78d
jeyavenkatesh-kumar <jeyavenkatesh.kumar@salesforce.com>
2024-10-07 17:05:22 +0530

    Refactor main branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import StarRating from "./StarRating";
-import styles from "./StarRating.module.css";
 
 export default function App() {
-  return (
-    <div className={styles.App}>
-      <StarRating value={0} total={5} />
-    </div>
-  );
+  return ();
 }
